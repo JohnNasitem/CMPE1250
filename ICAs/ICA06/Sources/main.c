e17973a687988f44c809e7f9b83d1067dd538bae
JohnNasitem <jnasitem1@nait.ca>
2024-03-05 10:03:59 -0700

    Added Skeleton file

--- ICAs/ICA06/Sources/main.c
+++ ICAs/ICA06/Sources/main.c
@@ -1,21 +1,72 @@
+/********************************************************************/
+// HC12 Program:  YourProg - MiniExplanation
+// Processor:     MC9S12XDP512
+// Bus Speed:     MHz
+// Author:        This B. You
+// Details:       A more detailed explanation of the program is entered here               
+// Date:          Date Created
+// Revision History :
+//  each revision will have a date + desc. of changes
+
+
+
+/********************************************************************/
+// Library includes
+/********************************************************************/
 #include <hidef.h>      /* common defines and macros */
-#include "derivative.h"      /* derivative-specific definitions */
+#include "derivative.h" /* derivative-specific definitions */
+
+//Other system includes or your includes go here
+//#include <stdlib.h>
+//#include <stdio.h>
 
 
+/********************************************************************/
+//Defines
+/********************************************************************/
 
+/********************************************************************/
+// Local Prototypes
+/********************************************************************/
 
+/********************************************************************/
+// Global Variables
+/********************************************************************/
 
+/********************************************************************/
+// Constants
+/********************************************************************/
 
-void main(void) {
-  /* put your own code here */
+/********************************************************************/
+// Main Entry
+/********************************************************************/
+void main(void)
+{
+  //Any main local variables must be declared here
+
+  // main entry point
+  _DISABLE_COP();
+  //EnableInterrupts;
   
+/********************************************************************/
+  // one-time initializations
+/********************************************************************/
 
 
-	EnableInterrupts;
+/********************************************************************/
+  // main program loop
+/********************************************************************/
 
+  for (;;)
+  {
 
-  for(;;) {
-    _FEED_COP(); /* feeds the dog */
-  } /* loop forever */
-  /* please make sure that you never leave main */
+  }                   
 }
+
+/********************************************************************/
+// Functions
+/********************************************************************/
+
+/********************************************************************/
+// Interrupt Service Routines
+/********************************************************************/
