ca2b9afcde6dea80044d3e8730469fb83b616252
JohnNasitem <jnasitem1@nait.ca>
2024-03-21 11:04:00 -0600

    completed part a and part b theorhetically

--- ICAs/ICA09/Sources/main.c
+++ ICAs/ICA09/Sources/main.c
@@ -19,6 +19,7 @@
 #include "clock.h"
 #include "sci.h"
 #include "rti.h"
+#include <string.h> 
 
 //Other system includes or your includes go here
 #include <stdlib.h>
@@ -36,6 +37,7 @@
 /********************************************************************/
 // Global Variables
 /********************************************************************/
+char[] vowels = ["AEIOUY"]
 
 /********************************************************************/
 // Constants
@@ -59,6 +61,11 @@ SWL_Init();
 Clock_EnableOutput(ClockOutDiv2);
 sci0_Init(19200, 0);
 RTI_Init();
+unsigned int counter = 0;
+unsigned char currentVowel;
+char[] randomString;
+unsigned int sumVowels = 0;
+unsigned int totalStringsTransmitted = 0;
 
 /********************************************************************/
   // main program loop
@@ -66,7 +73,38 @@ RTI_Init();
 
   for (;;)
   {
+    //Measure how long a transmission takes
+    SWL_ON(SWL_RED);
+    totalStringsTransmitted++;
+    counter = 0;
 
+    //Iterate 20 times
+    while (counter < 20) {
+      //Gets a random vowel
+      currentVowel = vowels[GetRandom(0, strlen(vowels))];
+
+      //Uppercase the vowel if center button is pressed and lowercase if not
+      if (SWL_Pushed(SWL_CTR)) currentVowel = touppper(currentVowel); //check if toupper works like that
+      else currentVowel = tolower(currentVowel);
+
+      randomString[counter] = currentVowel;
+
+      sumVowels += currentVowel; //Check to make sure this adds the vowels instead of concatinate
+
+      counter++;
+    }
+
+    //Part B: adds the sum of the vowels after the vowels with a space before and after
+    randomString[counter] " %04d ", sumVowels;
+
+    //Display on the screen
+    sci0_txStr(&randomString);
+
+
+    SWL_OFF(SWL_RED);
+
+    //Delay by 250ms
+    RTI_Delay_ms(250);
   }                   
 }
 
