0b8fdebf525cf65067e29a86480da3cbec3fc24f
John Nasitem <jnasitem1@nait.ca>
2024-03-29 08:31:16 -0600

    Commented the programmer block

--- ICAs/ICA09/Sources/main.c
+++ ICAs/ICA09/Sources/main.c
@@ -1,9 +1,9 @@
 /********************************************************************/
-// HC12 Program:  YourProg - MiniExplanation
+// HC12 Program:  Ica 09
 // Processor:     MC9S12XDP512
-// Bus Speed:     MHz
-// Author:        This B. You
-// Details:       A more detailed explanation of the program is entered here               
+// Bus Speed:     40 MHz
+// Author:        John N. Nasitem
+// Details:       send in a random string of vowels 20 chars long with the sum of hte vowels and run count              
 // Date:          Date Created
 // Revision History :
 //  each revision will have a date + desc. of changes
